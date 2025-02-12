9eb2e86f703926dc5a9acf9ae216c38b218654b3
akiidjk <dragonfaiver@gmail.com>
2025-02-12 15:43:03 +0100

    Fix match availability check to allow access when matches are available

--- backend/internal/server/routes/user_route.go
+++ backend/internal/server/routes/user_route.go
@@ -93,7 +93,7 @@ func (h *Handler) AddUserInfo(c *gin.Context) {
 func (h *Handler) GetMatches(c *gin.Context) {
 	log.Debug().Msgf("Time release: %s", timeRelease)
 
-	if timeRelease.After(time.Now()) {
+	if !timeRelease.After(time.Now()) {
 		c.JSON(http.StatusForbidden, gin.H{"error": "Matches are not available yet"})
 		return
 	}
